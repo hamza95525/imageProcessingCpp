fcebf953f4d1466a1c2d02bd5de94d5c03cab89b
Khamzat <khamzat@localhost.localdomain>
2020-05-07 02:18:43 +0200

    adding conversion from image to 2D array

--- main/main.cpp
+++ main/main.cpp
@@ -3,14 +3,35 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 int main() {
-    const std::string name = "../portrait-of-a-wolf.jpg";
+    std::string name;
+    std::cout << "Prosze wprowadzic sciezke do pliku: ";
+    std::cin >> name;
+
     cv::Mat img = cv::imread(name);
+    cv::Mat dst;
 
     if(!img.data){
         std::cout<<"Nie odnaleziono pliku!" << name << std::endl;
         return -1;
     }
 
+    img.convertTo(dst, CV_8U); //CV_8U == unsigned 8bit/pixel
+    float arrOut[dst.rows][dst.cols]; //2D array of
+
+    //pass data from picture to 2D array
+    for(int i=0; i<dst.rows; ++i)
+        for(int j=0; j<dst.cols; ++j)
+            arrOut[i][j] = dst.at<float>(i, j);
+
+    //show the array
+    for(int i = 0; i < dst.rows; ++i)
+        for(int j=0; j<dst.cols; ++j){
+            std::cout << arrOut[i][j]<<" ";
+            if(j==(dst.cols-1))
+                std::cout<<std::endl;
+        }
+
+    //show the picture
     const std::string window_name = "OpenCV";
     cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
     cv::imshow(window_name, img);
